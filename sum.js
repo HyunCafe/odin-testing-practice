a02b39e76b7808827010bd9733e99afb7e51b5e5
HyunCafe <eparksuncafe@gmail.com>
2023-05-10 16:32:12 -0500

    Add: added more example tests and conditions

--- sum.js
+++ sum.js
@@ -1,4 +1,36 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
-module.exports = sum;
+
+const capitalizeWord = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+const reverseString = (string) => {
+  return string.split("").reverse().join("");
+};
+
+const calculator = (expression) => {
+  const [a, operator, b] = expression.split(" ");
+  switch (operator) {
+    case "+":
+      return +a + +b;
+    case "-":
+      return +a - +b;
+    case "/":
+      return +a / +b;
+    case "*":
+      return +a * +b;
+    default:
+      return "nothing inputted";
+  }
+};
+
+const caesarCipher = (str, shift) => {
+    return str.replace(/[a-z]/ig, (char) => {
+      const base = char <= "Z" ? "A".charCodeAt(0) : "a".charCodeAt(0);
+      return String.fromCharCode(((char.charCodeAt(0) - base + shift) % 26) + base);
+    });
+  }
+
+module.exports = { sum, capitalizeWord, reverseString, calculator, caesarCipher };
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,24 @@
-const sum = require('./sum');
+const {sum, capitalizeWord, reverseString, calculator, caesarCipher} = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('capitalize first letter of word', () => {
+    expect(capitalizeWord('cat')).toBe('Cat');
+})
+
+test('reverse a string', () => {
+    expect(reverseString('this is a string')).toBe('gnirts a si siht');
+})
+
+test('calculate add, subtract, divide, multiply', () => {
+    expect(calculator("2 + 2")).toBe(4);
+    expect(calculator("2 - 2")).toBe(0);
+    expect(calculator("2 / 2")).toBe(1);
+    expect(calculator("2 * 2")).toBe(4);
+  });
+  
+  test('caesarCipher shift factor', () => {
+    expect(caesarCipher("Hello, World!", 3)).toBe('Khoor, Zruog!');
+  });
